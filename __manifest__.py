93defe6a0598b3b4c9e8d1a690555d5b7a01e119
mvidalt <mvidalt@cifpfbmoll.eu>
2024-01-23 20:49:27 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Gestion de biblioteca",
+    'summary': """Gestión del catálogo de la biblioteca y los prestamos de libros """,
+    'author': "Miguel Vidal Tordillo",
+    'license': "AGPL-3",
+    'website': "https://github.com/mvidalt",
+    'version': '1.0',
+    'depends': ['base'],
+    'application': True,
+    'category': "Services/Library",
+
+    "data": [
+        "security/library_security.xml",
+        "security/ir.model.access.csv",
+        "views/book_view.xml",
+        "views/library_menu.xml",
+    ]
+}
